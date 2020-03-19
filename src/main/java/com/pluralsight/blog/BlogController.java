23a554b20d39d384e740fa702ef89529a0ba4a47
Suman Khara <suman.khara@cerner.com>
2020-03-19 16:49:54 +0530

    add posts

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,28 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 @Controller
 public class BlogController {
+	
+	private PostRepository postRepository;
+
+	public BlogController(PostRepository postRepository) {
+		super();
+		this.postRepository = postRepository;
+	}
 
 	@RequestMapping("/")
 	public String listPosts(ModelMap model) {
-		model.put("title", "Blog Post 1");
+		List<Post> posts = postRepository.getAllPosts();
+		model.put("posts", posts);
 		return "home";
 	}
 }
